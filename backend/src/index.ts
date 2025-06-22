5f636929b8f5aa1173c60c8b52dbfe8f04bd3f13
KRRISH AGARWAL <krrishapj@gmail.com>
2025-06-23 02:51:50 +0530

    added the authMiddleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,9 +2,28 @@ import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 import { Hono } from "hono";
 import { env } from "hono/adapter";
+import { createMiddleware } from "hono/factory";
 import { sign, verify } from "hono/jwt";
 
 const app = new Hono();
+
+const authMiddleware = createMiddleware(async (c, next) => {
+  try {
+    const authHeader = c.req.header("Authorization") || "";
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return c.json({ error: "Not authorized" }, 411);
+    }
+
+    const token = authHeader.split(" ")[1];
+    const { JWT_SECRET } = env<{ JWT_SECRET: string }>(c);
+    const { id } = await verify(token, JWT_SECRET);
+    c.set("id", id);
+    await next();
+  } catch (error) {
+    console.log(error);
+    return c.json({ msg: "error in middleware" });
+  }
+});
 
 app.post("/api/v1/user/signup", async (c) => {
   try {
@@ -29,7 +48,7 @@ app.post("/api/v1/user/signup", async (c) => {
     return c.json({ token });
   } catch (error) {
     console.log(error);
-    return c.json({ msg: "ERROR occurred" }, 500);
+    return c.json({ msg: "ERROR in signup" }, 500);
   }
 });
 
@@ -58,19 +77,19 @@ app.post("/api/v1/user/signin", async (c) => {
     return c.json({ token });
   } catch (error) {
     console.log(error);
-    return c.json({ msg: "ERROR occurred" }, 500);
+    return c.json({ msg: "ERROR in signin" }, 500);
   }
 });
 
 app.post("/api/v1/blog/", (c) => {
-  return c.text("signup route");
+  return c.text("blog post");
 });
 
 app.put("/api/v1/blog/", (c) => {
-  return c.text("signup route");
+  return c.text("blog put");
 });
 
-app.get("/api/v1/blog/bulk", (c) => {
+app.get("/api/v1/blog/bulk", authMiddleware, (c) => {
   return c.text("blog bulk route");
 });
 
