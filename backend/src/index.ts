ca4042ce7a408184612955316060963deb8fa1ac
KRRISH AGARWAL <krrishapj@gmail.com>
2025-06-23 00:47:02 +0530

    earlier commit was flawed, Removed that folder comletely, added a fresh one, started hono, added prisma, migrated safely, generated, integrated accelerate url, and got the env in index.txt

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,23 @@
-import { Hono } from 'hono'
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { Hono } from "hono";
+import { env } from "hono/adapter";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", async (c) => {
+  const { DATABASE_URL } = env<{ DATABASE_URL: string }>(c);
+  const prisma = new PrismaClient({ datasourceUrl: DATABASE_URL }).$extends(
+    withAccelerate()
+  );
+  const res = await prisma.user.create({
+    data: {
+      email: "temp@gmail.com",
+      name: "temp",
+      password: "tempp",
+    },
+  });
+  console.log("DONNENNENE", res, DATABASE_URL);
+});
 
-export default app
+export default app;
